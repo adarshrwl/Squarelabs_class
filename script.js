20e0424621cb8df635fa426272e95bb330f47196
Adarsh Rawal <adarshrawal321@gmail.com>
2025-12-02 19:59:26 +0545

    javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Javascript Running!!");
+
+const pi = 3.13;
+
+pi = 123;
+
+console.log(pi);
+
+let age = 23;
+
+if (age >= 18) {
+  console.log("You can vote");
+} else {
+  console.log(" You cannot vote");
+}
