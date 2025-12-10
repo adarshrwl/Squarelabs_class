4d7e3767ffa2fe88c22127115f121b60370dd9ab
Adarsh Rawal <adarshrawal321@gmail.com>
2025-12-10 21:15:18 +0545

    API CREATED

--- my-mern-application/backend/index.js
+++ my-mern-application/backend/index.js
@@ -1,1 +1,14 @@
-console.log("Hello");
+const express = require("express");
+const app = express();
+
+app.get("/health", (req, res) => {
+  res.send("Status Okay!!");
+});
+
+app.listen(5000, () => {
+  console.log("Server is running");
+});
+
+const authroutes = require("./routes/authroutes");
+app.use("/auth", authroutes);
+//login api-localhost:5000/auth/login
