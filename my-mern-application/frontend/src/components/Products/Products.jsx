372cf7c71ce215d7962202c24348d2991e0cc54e
Adarsh Rawal <adarshrawal321@gmail.com>
2026-01-01 19:35:12 +0545

    class revision

--- my-mern-application/frontend/src/components/Products/Products.jsx
+++ my-mern-application/frontend/src/components/Products/Products.jsx
@@ -6,6 +6,11 @@ const Products = () => {
   //usestate of products
   const [products, setProducts] = useState([]);
 
+  // Notes
+  // backend working
+  // async await=api call database time lange
+  // async await use hudaina -use effect can be used!!
+  // useEffect
   useEffect(() => {
     axios
       .get("http://localhost:5000/getProducts")
@@ -15,6 +20,39 @@ const Products = () => {
       })
       .catch((err) => console.error(err));
   }, []);
+  // p1
+  // {
+  // id
+  //   product1;
+  // }
+  // p2
+  // {
+  // id
+  //   product2;
+  // }
+  // p3
+  // {
+  // id
+  //   product3;
+  // }
+  //frontend rendering !!!
+
+  /*
+  //example
+  product =[1,2,3,4,5,]
+  product.map((p)={{console.log(product.id)}})
+  1st turn
+  1st element=1
+  output 1,=product cardP{json data}//task sakyo
+  2
+  3
+  4
+  5
+
+
+
+  
+  */
 
   return (
     <div className="container mt-3">
